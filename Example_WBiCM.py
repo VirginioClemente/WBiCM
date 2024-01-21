095a197ce1ef927a626d499db0d921e928f9f8bd
Giulio Virginio Clemente <g.v.clemente@outlook.it>
2024-01-21 15:04:07 +0100

    Update Example_WBiCM.py

--- Example_WBiCM.py
+++ Example_WBiCM.py
@@ -1,15 +1,15 @@
 from Weighted_Bipartite import weighted_bipartite
 
 
-# Importo la matrice della rete bipartita pesata
+# Importing the weighted bipartite network matrix
 weighted_matrix = np.loadtxt('bicm_matW.csv', delimiter=',',dtype=str)
 weighted_matrix =  weighted_matrix[1:, 1:].astype(float)
 
 
-# Definisco la parte binaria
+# Defining the binary part
 topologia = weighted_matrix.astype(bool).astype(int)
 
-# Calcolo le statistiche che mi interessano
+# Calculating the statistics of interest
 s_row = weighted_matrix.sum(1)
 s_col = weighted_matrix.sum(0)
 
@@ -17,19 +17,19 @@ k_row = weighted_matrix.astype(bool).astype(int).sum(1)
 k_col = weighted_matrix.astype(bool).astype(int).sum(0)
 
 
-# Inizializzo la classe:
+# Initializing the class:
 
-# 1 Se la topologia non è nota.
+# 1 If the topology is not known.
 bipartito = weighted_bipartite(s_row,s_col,k_rows=k_row,k_cols=k_col)
 
-# 2 Se la topologia è nota
+# 2 If the topology is known
 #bipartito = weighted_bipartite(s_row,s_col,topology=topologia)
 
-# Trovo la soluzione al problema
+# Finding the solution to the problem
 bipartito.solve()
 
 beta = bipartito.beta
 alfa = bipartito.alfa
 
-# Creo la cartella "samples_WBiCM" con 50 campioni della rete al suo interno
+# Creating the folder 'samples_WBiCM' with 50 samples of the network inside
 bipartito.sampler(50)
